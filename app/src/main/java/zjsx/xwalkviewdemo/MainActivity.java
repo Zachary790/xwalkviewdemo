39530810f9c067c414aea1074ece1fca59a90667
di.zhang <eddieeezhang@outlook.com>
2016-04-21 12:49:08 +0800

    INIT

---
+++ app/src/main/java/zjsx/xwalkviewdemo/MainActivity.java
@@ -0,0 +1,77 @@
+package zjsx.xwalkviewdemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebResourceResponse;
+
+import org.xwalk.core.XWalkPreferences;
+import org.xwalk.core.XWalkResourceClient;
+import org.xwalk.core.XWalkUIClient;
+import org.xwalk.core.XWalkView;
+
+public class MainActivity extends AppCompatActivity {
+    XWalkView mXwalkView;
+
+    class MyResourceClient extends XWalkResourceClient {
+        MyResourceClient(XWalkView view) {
+            super(view);
+        }
+    }
+
+    class MyUIClient extends XWalkUIClient {
+        MyUIClient(XWalkView view) {
+            super(view);
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mXwalkView = (XWalkView) findViewById(R.id.xwalkView);
+        mXwalkView.setResourceClient(new MyResourceClient(mXwalkView));
+        mXwalkView.setUIClient(new MyUIClient(mXwalkView));
+        mXwalkView.load("http://www.bing.com", null);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mXwalkView != null) {
+            mXwalkView.pauseTimers();
+            mXwalkView.onHide();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mXwalkView != null) {
+            mXwalkView.resumeTimers();
+            mXwalkView.onShow();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mXwalkView != null) {
+            mXwalkView.onDestroy();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (mXwalkView != null) {
+            mXwalkView.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        if (mXwalkView != null) {
+            mXwalkView.onNewIntent(intent);
+        }
+    }
+}
